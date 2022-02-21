1f597bb98968b63c70f9b139531fe7e16da31354
virgenensaturno <virginia.gallo1@gmail.com>
2022-02-21 16:49:00 -0300

    correción de lógica de bucles y swithc

--- BonusEj1.js
+++ BonusEj1.js
@@ -1,5 +1,5 @@
 //Realizar un programa que, ingresando la letra de la categoría de
-//Monotributo, permita obtener algunos de sus datos. Quiero lograrlo con HTML formularios//
+//Monotributo, permita obtener algunos de sus datos. //
 const afip=[
     {
     categoria: "A",
@@ -15,15 +15,38 @@ const afip=[
     categoria: "C",
     IngresosBrutos: 970203.30,
     Actividad: "No excluida",
-    }
+    },
 ]
-var consulta= window.prompt("Ingrese su catergoría de Monotributo")
-if (consulta== "a"|| consulta=="A"){
-    var consultaB= window.prompt("Indique la opción deseada: A- Ingresos Brutos. B-Actividad.")
-        switch(afip){
-            case 'A': alert('Usted está en la categoría'+categoria+' y sus ingresos brutos son'+IngresosBrutos);
-                break;
-            case 'B':alert('Usted está en la categoría'+categoria+' y su actividad es '+Actividad);
-                break;
+var consulta= window.prompt("Ingrese su catergoría de Monotributo: A,B o C")
+switch(consulta){
+    case 'A'|| 'a':  var consultaB= window.prompt("Indique la opción deseada: A- Ingresos Brutos. B-Actividad."),
+        if (consultaB=="a"|| consultaB=="A"){
+            console.log('Usted está en la categoría'+categoria+' y sus ingresos brutos son'+IngresosBrutos),
+        };
+        else if (consultaB=="b"|| consultaB=="B"){
+            console.log('Usted está en la categoría'+categoria+' y su actividad es '+Actividad), 
+        },
+        else {
+            console.log("Opción incorrecta, ingrese los datos nuevamente."),
+        },
+    case 'b'|| 'B':  var consultaC= window.prompt("Indique la opción deseada: A- Ingresos Brutos. B-Actividad."),
+        if (consultaC=="a"|| consultaC=="A"){
+            console.log('Usted está en la categoría'+categoria+' y sus ingresos brutos son'+IngresosBrutos), 
         }
-    }
+        else if (consultaC=="b"|| consultaC=="B"){
+            console.log('Usted está en la categoría'+categoria+' y su actividad es '+Actividad), 
+        },
+        else {
+            console.log("Opción incorrecta, ingrese los datos nuevamente."),
+        },
+        case 'c'||'C':  var consultaD= window.prompt("Indique la opción deseada: A- Ingresos Brutos. B-Actividad.")
+        if (consultaD=="a"|| consultaD=="A"){
+            console.log('Usted está en la categoría'+categoria+' y sus ingresos brutos son'+IngresosBrutos),
+        },
+        else if (consultaD=="b"|| consultaD=="B"){
+            console.log('Usted está en la categoría'+categoria+' y su actividad es '+Actividad), 
+        },
+        else{
+            console.log("Opción incorrecta, ingrese los datos nuevamente."),
+        }
+};
